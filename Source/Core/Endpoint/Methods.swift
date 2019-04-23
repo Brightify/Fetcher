5a45ed44d4add58e414d2301285d2a51c62d97e8
Marek Slavik <m.slavik22@gmail.com>
2019-04-23 14:53:07 +0200

    Public class fix.

--- Source/Core/Endpoint/Methods.swift
+++ Source/Core/Endpoint/Methods.swift
@@ -6,63 +6,63 @@
 //  Copyright © 2016 Brightify. All rights reserved.
 //
 
-public class CONNECT<IN, OUT>: Endpoint<IN, OUT> {
+open class CONNECT<IN, OUT>: Endpoint<IN, OUT> {
     
     public override static var method: HTTPMethod {
         return .connect
     }
 }
 
-public class DELETE<IN, OUT>: Endpoint<IN, OUT> {
+open class DELETE<IN, OUT>: Endpoint<IN, OUT> {
     
     public override static var method: HTTPMethod {
         return .delete
     }
 }
 
-public class GET<IN, OUT>: Endpoint<IN, OUT> {
+open class GET<IN, OUT>: Endpoint<IN, OUT> {
     
     public override static var method: HTTPMethod {
         return .get
     }
 }
 
-public class HEAD<IN, OUT>: Endpoint<IN, OUT> {
+open class HEAD<IN, OUT>: Endpoint<IN, OUT> {
     
     public override static var method: HTTPMethod {
         return .head
     }
 }
 
-public class OPTIONS<IN, OUT>: Endpoint<IN, OUT> {
+open class OPTIONS<IN, OUT>: Endpoint<IN, OUT> {
     
     public override static var method: HTTPMethod {
         return .options
     }
 }
 
-public class PATCH<IN, OUT>: Endpoint<IN, OUT> {
+open class PATCH<IN, OUT>: Endpoint<IN, OUT> {
     
     public override static var method: HTTPMethod {
         return .patch
     }
 }
 
-public class POST<IN, OUT>: Endpoint<IN, OUT> {
+open class POST<IN, OUT>: Endpoint<IN, OUT> {
     
     public override static var method: HTTPMethod {
         return .post
     }
 }
 
-public class PUT<IN, OUT>: Endpoint<IN, OUT> {
+open class PUT<IN, OUT>: Endpoint<IN, OUT> {
     
     public override static var method: HTTPMethod {
         return .put
     }
 }
 
-public class TRACE<IN, OUT>: Endpoint<IN, OUT> {
+open class TRACE<IN, OUT>: Endpoint<IN, OUT> {
     
     public override static var method: HTTPMethod {
         return .trace
